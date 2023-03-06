7c1f97cfaf3c75e6dfb932102cb858c6e09c2f52
huydev0401 <quanghuynguyennqh@gmail.com>
2023-03-06 11:22:14 +0700

    qrcode project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Form, Input, Space, QRCode } from "antd";
+import { useRef, useState } from "react";
 
 function App() {
+  const [form] = Form.useForm();
+  const [qrValues, setQRValues] = useState("");
+  const qrCodeRef = useRef(null);
+
+  const onFinish = (values) => {
+    setQRValues(values.url);
+  };
+
+  const handleDownload = () => {
+    const canvas = qrCodeRef.current.querySelector("canvas");
+    const downloadLink = document.createElement("a");
+    downloadLink.download = "qrcode.png";
+    downloadLink.href = canvas.toDataURL("image/png");
+    downloadLink.click();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex items-center justify-center">
+      <div className="max-w-[500px] mt-[120px] shadow-md p-[40px] rounded-3xl bg-white">
+        <h2 className="uppercase text-[32px] font-semibold mb-[20px] text-center font-sans">
+          Tạo QR Code tự động
+        </h2>
+        <Form
+          form={form}
+          layout="vertical"
+          onFinish={onFinish}
+          autoComplete="off"
+          className="w-full"
         >
-          Learn React
-        </a>
-      </header>
+          <Form.Item
+            name="url"
+            label="URL"
+            rules={[
+              { required: true },
+              { type: "url", warningOnly: true },
+              { type: "string", min: 6 },
+            ]}
+          >
+            <Input placeholder="Nhập URL của bạn!" />
+          </Form.Item>
+          <Form.Item>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                Generator QRCode
+              </Button>
+            </Space>
+          </Form.Item>
+          {qrValues && (
+            <div
+              ref={qrCodeRef}
+              className="flex flex-col items-center mt-[50px]"
+            >
+              <h4>Mã QR Code của bạn</h4>
+              <QRCode value={qrValues} className="mb-[20px]" />
+              <Button onClick={handleDownload}>Download QR Code</Button>
+            </div>
+          )}
+        </Form>
+        <p className="text-[10px] font-sans italic text-right">make by Ethan</p>
+      </div>
     </div>
   );
 }
